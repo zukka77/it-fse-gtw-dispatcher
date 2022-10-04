94cd9d01f55efc2184844d52b7e988989f9266ad
Simone <simonelungarella@gmail.com>
2022-10-04 15:58:00 +0200

    fix: fixed enums values in structured logs

--- src/main/java/it/finanze/sanita/fse2/ms/gtw/dispatcher/enums/OperationLogEnum.java
+++ src/main/java/it/finanze/sanita/fse2/ms/gtw/dispatcher/enums/OperationLogEnum.java
@@ -6,9 +6,9 @@ public enum OperationLogEnum implements ILogEnum {
 
 	VAL_CDA2("VAL-CDA2", "Validazione CDA2"),
 	PUB_CDA2("PUB-CDA2", "Pubblicazione CDA2"),
-	REPLACE_CDA2("REP-CDA2", "Replace CDA2"),
-	DELETE_CDA2("DEL-CDA2","Delete CDA2"),
-	UPDATE_METADATA_CDA2("UPD-MTD-CDA2","Update metadata CDA2"),
+	REPLACE_CDA2("REPLACE-CDA2", "Replace CDA2"),
+	DELETE_CDA2("DELETE-CDA2","Delete CDA2"),
+	UPDATE_METADATA_CDA2("UPDATE-CDA2","Update metadata CDA2"),
 	TRAS_CDA2("TRAS-CDA2", "Trasmissione CDA2"),
 	KAFKA_SENDING_MESSAGE("KAFKA-SENDING-MESSAGE", "Invio Messaggio su Kafka"),
 	REDIS("REDIS", "Salvataggio/Query su Redis");
