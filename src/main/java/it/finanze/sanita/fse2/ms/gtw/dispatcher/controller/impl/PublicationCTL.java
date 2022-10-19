0dcc226643d1d459356eb99790a05d22c0e4a572
Simone Le Noci <simonelenocidrum@gmail.com>
2022-10-19 10:29:22 +0200

    feat : added license

--- src/main/java/it/finanze/sanita/fse2/ms/gtw/dispatcher/controller/impl/PublicationCTL.java
+++ src/main/java/it/finanze/sanita/fse2/ms/gtw/dispatcher/controller/impl/PublicationCTL.java
@@ -1,3 +1,6 @@
+/*
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
 package it.finanze.sanita.fse2.ms.gtw.dispatcher.controller.impl;
 
 import java.util.Date;
--- src/main/java/it/finanze/sanita/fse2/ms/gtw/dispatcher/exceptions/ValidationException.java
+++ src/main/java/it/finanze/sanita/fse2/ms/gtw/dispatcher/exceptions/ValidationException.java
@@ -1,5 +1,5 @@
-/**
- * 
+/*
+ * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 package it.finanze.sanita.fse2.ms.gtw.dispatcher.exceptions;
 
