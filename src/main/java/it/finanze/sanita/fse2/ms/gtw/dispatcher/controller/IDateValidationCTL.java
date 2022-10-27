cdf569f379ecebf1758cd36e81f50f6fe9373ed8
Simone Le Noci <simonelenocidrum@gmail.com>
2022-10-27 10:04:37 +0200

    fix : nullPointer & code smells

--- src/main/java/it/finanze/sanita/fse2/ms/gtw/dispatcher/controller/IDateValidationCTL.java
+++ src/main/java/it/finanze/sanita/fse2/ms/gtw/dispatcher/controller/IDateValidationCTL.java
@@ -3,8 +3,8 @@ package it.finanze.sanita.fse2.ms.gtw.dispatcher.controller;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -20,7 +20,7 @@ import it.finanze.sanita.fse2.ms.gtw.dispatcher.dto.ValidatedDocumentsDTO;
 @Tag(name = "Test CTL")
 public interface IDateValidationCTL {
 	
-	@RequestMapping(value = "/documents/updateValidationDate", method = RequestMethod.PUT, produces = { MediaType.APPLICATION_JSON_VALUE }, consumes = { MediaType.APPLICATION_JSON_VALUE })
+	@PutMapping(value = "/documents/updateValidationDate", produces = { MediaType.APPLICATION_JSON_VALUE }, consumes = { MediaType.APPLICATION_JSON_VALUE })
 	@ApiResponse(content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ValidatedDocumentsDTO.class)))
 	@Operation(summary = "End point di test", description = "Ep di test per modifica la data di validazione al fine di pubblicare dopo 5 giorni.")
 	@ApiResponses(value = {
--- src/main/java/it/finanze/sanita/fse2/ms/gtw/dispatcher/controller/impl/DateValidationCTL.java
+++ src/main/java/it/finanze/sanita/fse2/ms/gtw/dispatcher/controller/impl/DateValidationCTL.java
@@ -8,8 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 import it.finanze.sanita.fse2.ms.gtw.dispatcher.controller.IDateValidationCTL;
 import it.finanze.sanita.fse2.ms.gtw.dispatcher.exceptions.BusinessException;
 import it.finanze.sanita.fse2.ms.gtw.dispatcher.service.IDateValidationSRV;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
+@Slf4j
 public class DateValidationCTL extends AbstractCTL implements IDateValidationCTL {
 
 	@Autowired
@@ -21,6 +23,7 @@ public class DateValidationCTL extends AbstractCTL implements IDateValidationCTL
 		try {
 			return dateValidationSRV.updateValidationDate(workflowInstanceId, days);
 		} catch (Exception ex) {
+			log.error("Error update validated document while srv invocation event : " , ex);
 			throw new BusinessException("Error update validated document while srv invocation event : ", ex);
 		}
 		
