e6012119be349edb7b262f8dd0800622a03712e2
Vincenzo Ingenito <54367888+vincenzo-ingenito@users.noreply.github.com>
2023-02-09 14:20:27 +0100

    Update PDFUtility.java

--- src/main/java/it/finanze/sanita/fse2/ms/gtw/dispatcher/utility/PDFUtility.java
+++ src/main/java/it/finanze/sanita/fse2/ms/gtw/dispatcher/utility/PDFUtility.java
@@ -4,17 +4,14 @@
 package it.finanze.sanita.fse2.ms.gtw.dispatcher.utility;
 
 import java.io.ByteArrayInputStream;
-import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamReader;
-import javax.xml.xpath.XPathExpressionException;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDDocumentCatalog;
@@ -23,7 +20,6 @@ import org.apache.pdfbox.pdmodel.PDEmbeddedFilesNameTreeNode;
 import org.apache.pdfbox.pdmodel.common.filespecification.PDComplexFileSpecification;
 import org.jsoup.Jsoup;
 import org.jsoup.parser.Parser;
-import org.xml.sax.SAXException;
 
 import com.lowagie.text.pdf.PRStream;
 import com.lowagie.text.pdf.PdfArray;
