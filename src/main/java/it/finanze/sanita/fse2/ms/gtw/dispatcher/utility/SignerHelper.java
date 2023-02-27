8e24be6fe48caffed3212336f18c596f019b6b51
Vincenzo Ingenito <54367888+vincenzo-ingenito@users.noreply.github.com>
2023-02-27 13:03:04 +0100

    feat: Add signer helper

---
+++ src/main/java/it/finanze/sanita/fse2/ms/gtw/dispatcher/utility/SignerHelper.java
@@ -0,0 +1,102 @@
+package it.finanze.sanita.fse2.ms.gtw.dispatcher.utility;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDDocumentCatalog;
+import org.apache.pdfbox.pdmodel.interactive.digitalsignature.PDSignature;
+import org.apache.pdfbox.pdmodel.interactive.form.PDAcroForm;
+import org.apache.pdfbox.pdmodel.interactive.form.PDField;
+import org.apache.pdfbox.pdmodel.interactive.form.PDSignatureField;
+import org.bouncycastle.cert.X509CertificateHolder;
+import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
+import org.bouncycastle.cms.CMSProcessableByteArray;
+import org.bouncycastle.cms.CMSSignedData;
+import org.bouncycastle.cms.SignerInformation;
+import org.bouncycastle.cms.SignerInformationVerifier;
+import org.bouncycastle.cms.jcajce.JcaSimpleSignerInfoVerifierBuilder;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import it.finanze.sanita.fse2.ms.gtw.dispatcher.dto.SignatureInfoDTO;
+import it.finanze.sanita.fse2.ms.gtw.dispatcher.dto.SignatureValidationDTO;
+import it.finanze.sanita.fse2.ms.gtw.dispatcher.exceptions.BusinessException;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class SignerHelper {
+
+	/******************************************************
+	*	VALIDATE PADES SIGNATURE
+	*******************************************************/
+	@SuppressWarnings("unchecked")
+	public static SignatureValidationDTO validate(byte[] file) {
+		SignatureValidationDTO output = null;
+		Boolean status = true;
+		List<SignatureInfoDTO> signatures = new ArrayList<>();
+		try {
+			PDDocument documentCDA = PDDocument.load(new ByteArrayInputStream(file));
+			PDDocumentCatalog docCatalog = documentCDA.getDocumentCatalog();
+			PDAcroForm acroForm = docCatalog.getAcroForm();
+			
+			if(acroForm!=null && acroForm.getFields()!=null) {
+				for (PDField field:acroForm.getFields()) {
+					if (field instanceof PDSignatureField) {
+						PDSignatureField pf = (PDSignatureField)field;
+						
+						PDSignature signature = pf.getSignature();
+						byte[] signatureAsBytes = signature.getContents(file);
+						byte[] signedContentAsBytes = signature.getSignedContent(file);
+						
+						CMSSignedData cms = new CMSSignedData(new CMSProcessableByteArray(signedContentAsBytes), signatureAsBytes);
+						SignerInformation signerInfo = (SignerInformation) cms.getSignerInfos().getSigners().iterator().next();
+						
+						X509CertificateHolder certHolder = (X509CertificateHolder) cms.getCertificates().getMatches(signerInfo.getSID()).iterator().next();
+						SignerInformationVerifier verifier = new JcaSimpleSignerInfoVerifierBuilder().setProvider(new BouncyCastleProvider()).build(certHolder);
+						X509Certificate cert = new JcaX509CertificateConverter().getCertificate(certHolder);
+						SignatureInfoDTO info = SignatureInfoDTO.builder().
+								principal(cert.getIssuerX500Principal()).
+								notBefore(cert.getNotBefore()).
+								notAfter(cert.getNotAfter()).
+								contactInfo(pf.getSignature().getContactInfo()).
+								fieldType(field.getFieldType()).
+								fullyQualifiedName(field.getFullyQualifiedName()).
+								location(pf.getSignature().getLocation()).
+								name(pf.getSignature().getName()).
+								reason(pf.getSignature().getReason()).
+								signDate(pf.getSignature().getSignDate().getTime()).
+								encrypAlgOID(signerInfo.getEncryptionAlgOID()).
+								digestAlgOID(signerInfo.getDigestAlgOID()).
+								valid(signerInfo.verify(verifier)).
+								build();
+						signatures.add(info);
+						status = info.getValid();
+					}
+				}
+			} else {
+				status = false;
+			}
+		} catch(Exception ex) {
+			log.error("Error while validate signature : " , ex);
+			status = false;
+		}
+		output = SignatureValidationDTO.builder().status(status).signatures(signatures).build(); 
+        return output;
+	}
+     
+	public static boolean isSigned(File file) {
+		boolean hasAnySignature = false;
+		try(PDDocument document = PDDocument.load(file);) {
+			hasAnySignature = !document.getSignatureDictionaries().isEmpty();
+		} catch (Exception e) {
+			log.error("Error while searching signature for file with name " + file.getName(), e);
+			throw new BusinessException("Error while searching signature for file with name " + file.getName(), e);
+		}
+
+		return hasAnySignature;
+	}
+
+}
