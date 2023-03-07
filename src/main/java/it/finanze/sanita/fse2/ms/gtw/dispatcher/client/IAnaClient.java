b99d708b044db89d8fad1eded2574092a6bd1913
Vincenzo Ingenito <54367888+vincenzo-ingenito@users.noreply.github.com>
2023-03-07 17:58:56 +0100

    feat: Remove unused classess

--- src/main/java/it/finanze/sanita/fse2/ms/gtw/dispatcher/client/IAnaClient.java
+++
@@ -1,9 +0,0 @@
-/*
- * SPDX-License-Identifier: AGPL-3.0-or-later
- */
-package it.finanze.sanita.fse2.ms.gtw.dispatcher.client;
-
-public interface IAnaClient {
-	
-	Boolean callAnaClient(String codFiscale);
-}
