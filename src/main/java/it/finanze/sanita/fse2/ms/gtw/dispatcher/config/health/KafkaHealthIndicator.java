3b6f283756c22b4b93ff0e3ee44fde1538c4dfcb
Vincenzo Ingenito <Vincenzo.Ingenito-CIC@ibm.com>
2023-03-28 20:05:46 +0200

    fix: Add catch Interrupted exception

--- src/main/java/it/finanze/sanita/fse2/ms/gtw/dispatcher/config/health/KafkaHealthIndicator.java
+++ src/main/java/it/finanze/sanita/fse2/ms/gtw/dispatcher/config/health/KafkaHealthIndicator.java
@@ -6,21 +6,29 @@ import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.stereotype.Component;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Component
+@Slf4j
 public class KafkaHealthIndicator implements HealthIndicator {
 
 	@Autowired
-    private AdminClient client;
-	
-    @Override
-    public Health health() {
-        Health health = null;
-        try {
-            client.listTopics().listings().get();
-            health = Health.up().build();
-        } catch (Exception e) {
-            health = Health.down(e).build();
-        }
-        return health;
-    }
+	private AdminClient client;
+
+	@Override
+	public Health health() {
+		Health health = null;
+		try {
+			client.listTopics().listings().get();
+			health = Health.up().build();
+		} catch (InterruptedException e) {
+			log.warn("Interrupted!", e);
+			health = Health.down(e).build();
+			// Restore interrupted state...
+			Thread.currentThread().interrupt();
+		} catch (Exception e) {
+			health = Health.down(e).build();
+		}
+		return health;
+	}
 }
