d45ba83d60c725a6fff362b10a652f74ff4a78c7
Vincenzo Ingenito <vincenzo.ingenito@hotmail.it>
2023-03-31 17:17:51 +0200

    Update KafkaHealthIndicator.java

--- src/main/java/it/finanze/sanita/fse2/ms/gtw/dispatcher/config/health/KafkaHealthIndicator.java
+++ src/main/java/it/finanze/sanita/fse2/ms/gtw/dispatcher/config/health/KafkaHealthIndicator.java
@@ -1,26 +1,26 @@
-package it.finanze.sanita.fse2.ms.gtw.dispatcher.config.health;
-
-import org.apache.kafka.clients.admin.AdminClient;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.actuate.health.Health;
-import org.springframework.boot.actuate.health.HealthIndicator;
-import org.springframework.stereotype.Component;
-
-@Component
-public class KafkaHealthIndicator implements HealthIndicator {
-
-	@Autowired
-    private AdminClient client;
-	
-    @Override
-    public Health health() {
-        Health health = null;
-        try {
-            client.listTopics().listings().get();
-            health = Health.up().build();
-        } catch (Exception e) {
-            health = Health.down(e).build();
-        }
-        return health;
-    }
-}
+//package it.finanze.sanita.fse2.ms.gtw.dispatcher.config.health;
+//
+//import org.apache.kafka.clients.admin.AdminClient;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.actuate.health.Health;
+//import org.springframework.boot.actuate.health.HealthIndicator;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class KafkaHealthIndicator implements HealthIndicator {
+//
+//	@Autowired
+//    private AdminClient client;
+//	
+//    @Override
+//    public Health health() {
+//        Health health = null;
+//        try {
+//            client.listTopics().listings().get();
+//            health = Health.up().build();
+//        } catch (Exception e) {
+//            health = Health.down(e).build();
+//        }
+//        return health;
+//    }
+//}
